e9202fa1d480223ab742051ddbe81b405e7f506f
asimon84 <ajsimon4@yahoo.com>
2025-10-17 12:10:51 -0700

    table fixes

--- resources/js/projects-table.js
+++ resources/js/projects-table.js
@@ -4,8 +4,8 @@ new DataTable('#projectsTable', {
     ajax: window.route,
     columns: [
         {data: 'id', name: 'id', searchable: true},
-        {data: 'name', name: 'string', searchable: true},
-        {data: 'description', name: 'text', className: 'truncate-text', searchable: true},
+        {data: 'name', name: 'name', searchable: true},
+        {data: 'description', name: 'description', className: 'truncate-text', searchable: true},
         {data: 'actions', name: 'actions', orderable: false, searchable: false}
     ]
 });
