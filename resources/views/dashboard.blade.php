19237d16e4443befdd70d846f344a3fbbce6e7e0
asimon84 <ajsimon4@yahoo.com>
2025-10-20 12:34:13 -0700

    style updates

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,30 +21,36 @@
         <br/>
 
         @if(!empty($unassignedTasks))
-            <h5>Unassigned Tasks</h5>
-
-            <ul id="mySortableList" class="list-group" data-project-id="null">
-                @foreach($unassignedTasks as $unassignedTask)
-                    <li class="list-group-item" data-task-id="{{ $unassignedTask->id }}"><strong>Name: </strong>{{ $unassignedTask->name }} &nbsp; <strong>Priority: </strong><span id="priority-task-{{ $unassignedTask->id }}">N/A</span></li>
-                @endforeach
-                &nbsp;
-            </ul>
-
-            <br/>
+            <div class="card">
+                <div class="card-header">
+                    Unassigned Tasks
+                </div>
+                <div class="card-body">
+                    <ul id="mySortableList" class="list-group" data-project-id="null">
+                        @foreach($unassignedTasks as $unassignedTask)
+                            <li class="list-group-item" data-task-id="{{ $unassignedTask->id }}"><strong>Name: </strong>{{ $unassignedTask->name }} &nbsp; <strong>Priority: </strong><span id="priority-task-{{ $unassignedTask->id }}">N/A</span></li>
+                        @endforeach
+                        &nbsp;
+                    </ul>
+                </div>
+            </div>
         @endif
 
         @if(!empty($projects))
             @foreach($projects as $project)
-                <h5>{{ $project->name }}</h5>
-
-                <ul id="mySortableList" class="list-group" data-project-id="{{ $project->id }}">
-                    @foreach($project->tasks as $task)
-                        <li class="list-group-item" data-task-id="{{ $task->id }}"><strong>Name: </strong>{{ $task->name }} &nbsp; <strong>Priority: </strong><span id="priority-task-{{ $task->id }}">{{ $task->priority }}</span></li>
-                    @endforeach
-                    &nbsp;
-                </ul>
-
-                <br/>
+                <div class="card">
+                    <div class="card-header">
+                        {{ $project->name }}
+                    </div>
+                    <div class="card-body">
+                        <ul id="mySortableList" class="list-group" data-project-id="{{ $project->id }}">
+                            @foreach($project->tasks as $task)
+                                <li class="list-group-item" data-task-id="{{ $task->id }}"><strong>Name: </strong>{{ $task->name }} &nbsp; <strong>Priority: </strong><span id="priority-task-{{ $task->id }}">{{ $task->priority }}</span></li>
+                            @endforeach
+                            &nbsp;
+                        </ul>
+                    </div>
+                </div>
             @endforeach
         @endif
     </div>
